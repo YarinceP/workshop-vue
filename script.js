6cdd92419b935276dfb26e821b8648334caa42e9
YarinceP <padilla.yarince@gmail.com>
2020-05-21 07:32:17 -0600

    create a method to add a new keep

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ new Vue({
     el: '#appVue',
     data:{
         list:[
-            {keep:'uno',completed:false},
-            {keep:'dos',completed:false},
-            {keep:'tres',completed:true},
+            
         ],
+        newKeep:'',
+    },
+    methods: {
+        addKeep: function(){
+            this.list.push({keep:this.newKeep,completed:false});
+            this.newKeep='';
+        }
     },
 });
