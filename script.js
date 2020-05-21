3742f298defbe292be9419d10524dce4ab607d4e
YarinceP <padilla.yarince@gmail.com>
2020-05-21 06:44:57 -0600

    create script.js file

---
+++ script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#appVue',
+    data:{
+        list:[
+            {keep:'uno',completed:false},
+            {keep:'dos',completed:false},
+            {keep:'tres',completed:true},
+        ],
+    },
+});
